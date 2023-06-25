be59b5fa6868f9b4d9214f928e7291996c804958
HowardMui <howardmui.2020@gmail.com>
2023-06-26 04:45:28 +0800

    feat: add app config

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export const API_URL = process.env.REACT_APP_API_URL || "https://howardmui-blog-portfolio.net";
+//   export const IMAGE_URL =
+//   process.env.REACT_APP_IMAGE_URL || 'https://static.appicidea.com';
+export const PAGE_SIZE = 20;
+export const TOAST_TIME = 5000;
+export const APP_NAME = "Howard's blog portfolio";
+export const IMAGE_SIZE = 1024 * 10000;
