8cca31959ef7e901e2a758a0f91e0851a70f9518
HowardMui <howardmui.2020@gmail.com>
2023-06-26 04:43:12 +0800

    feat: add Redux Provider in index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import { AxiosInterceptor } from "services";
 import Router from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <AxiosInterceptor>
-        <Router />
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Router />
+          </PersistGate>
+        </Provider>
       </AxiosInterceptor>
     </BrowserRouter>
   );
